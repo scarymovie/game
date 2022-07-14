1c968d8539b14506a0d370a945bcbe30f356a92e
scarymovie <81684836+scarymovie@users.noreply.github.com>
2022-07-14 16:56:48 +0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Question;
+use App\Models\Team;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -23,6 +24,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $category = Category::factory(10)->create();
+        Team::factory(50)->create();
         Question::factory(200)->create();
     }
 }
