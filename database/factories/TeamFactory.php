590aa33b2ad2044244cfe5e56b8944c5b3493e10
scarymovie <81684836+scarymovie@users.noreply.github.com>
2022-07-14 17:03:32 +0300

    seeder

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -17,7 +17,7 @@ class TeamFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->word
+            'name' => $this->faker->word,
         ];
     }
 }
