6711588271f2c7eb3916befa6cad0bf52e4bad9d
scarymovie <81684836+scarymovie@users.noreply.github.com>
2022-08-11 01:09:15 +0300

    авторизация по имени и паролю

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,9 +17,9 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="name"/>Name
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" required autofocus />
             </div>
 
             <!-- Password -->
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,13 +19,6 @@
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
             <!-- Password -->
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
